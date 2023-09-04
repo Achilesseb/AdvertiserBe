9b6414967998bace019ce07accc15b46943b252e
Prisacariu Victor-Sebastian <95617979+Achilesseb@users.noreply.github.com>
2023-09-04 10:13:48 +0300

    ADV-BE-13. Added query modifiers to add paginations. Implemented on promotions.

--- graphql/resolvers/clientsResolver.ts
+++ graphql/resolvers/clientsResolver.ts
@@ -21,18 +21,16 @@ export type EditClientInput = ClientInput & {
 
 export const clientsResolver = {
   Query: {
-    getAllClients: async () => getAllClients(),
-    getClientById: async (_: undefined, { clientId }: { clientId: string }) =>
+    getAllClients: () => getAllClients(),
+    getClientById: (_: undefined, { clientId }: { clientId: string }) =>
       getClientById(clientId),
   },
   Mutation: {
-    addNewClient: async (_: undefined, { input }: { input: ClientInput }) =>
+    addNewClient: (_: undefined, { input }: { input: ClientInput }) =>
       addNewClient(input),
-    editClient: async (_: undefined, { input }: { input: EditClientInput }) =>
+    editClient: (_: undefined, { input }: { input: EditClientInput }) =>
       editClient(input),
-    deleteClient: async (
-      _: undefined,
-      { clientIds }: { clientIds: Array<string> },
-    ) => deleteClient(clientIds),
+    deleteClient: (_: undefined, { clientIds }: { clientIds: Array<string> }) =>
+      deleteClient(clientIds),
   },
 };
---
+++ graphql/utils/modifiers.ts
@@ -0,0 +1,44 @@
+import { PostgrestFilterBuilder } from '@supabase/postgrest-js';
+import { GenericSchema } from '@supabase/supabase-js/dist/module/lib/types';
+
+const addPaginationModifiers = <ResultModel>(
+  baseQuery: PostgrestFilterBuilder<
+    GenericSchema,
+    Record<string, unknown>,
+    ResultModel
+  >,
+  pagination: {
+    entitiesPerPage: number;
+    page: number;
+  },
+) => {
+  const entitiesPerPage = pagination.entitiesPerPage ?? 10;
+  const page = pagination.page ?? 1;
+  const startingIndex = entitiesPerPage * (page - 1);
+  const endingIndex = entitiesPerPage * page - 1;
+
+  return baseQuery.range(startingIndex, endingIndex);
+};
+
+export const addQueryModifiers = async <ResultModel>(
+  baseQuery: PostgrestFilterBuilder<
+    GenericSchema,
+    Record<string, unknown>,
+    ResultModel
+  >,
+  modifiers: {
+    pagination: {
+      entitiesPerPage: number;
+      page: number;
+    };
+  },
+) => {
+  const modifiersMapper: Record<string, typeof addPaginationModifiers> = {
+    pagination: addPaginationModifiers,
+  };
+  return Object.entries(modifiers).reduce(
+    (query, [modifier, modifierArgs]) =>
+      modifiersMapper?.[modifier]<ResultModel>(query, modifierArgs),
+    baseQuery,
+  );
+};
