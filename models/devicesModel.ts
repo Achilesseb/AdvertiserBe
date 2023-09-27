a477907fabb61a998bd0f952e14361004f2b584c
Prisacariu Victor-Sebastian <95617979+Achilesseb@users.noreply.github.com>
2023-09-27 09:30:49 +0300

    Solvec conflicts

--- models/devicesModel.ts
+++ models/devicesModel.ts
@@ -319,3 +319,12 @@ export type DeviceActivityReturnType = {
   name: string;
   teamName: string;
 };
+
+export type DeviceActivityReturnType = {
+  deviceId: string;
+  lastTimeCreated: string;
+  latitude: number;
+  longitude: number;
+  name: string;
+  teamName: string;
+};
