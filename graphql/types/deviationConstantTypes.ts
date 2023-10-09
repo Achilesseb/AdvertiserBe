2a474aaa36e00939aaeed7db1a93d46bbff584fc
Victor Prisacariu <victor.prisacariu@evozon.com>
2023-10-09 16:28:07 +0300

    Fix:Changed deleteConstants type to match FE requirements

--- graphql/types/deviationConstantTypes.ts
+++ graphql/types/deviationConstantTypes.ts
@@ -5,26 +5,25 @@ export const deviationTypes = `#graphql
         identifier: String
         inUse: Boolean
     }
+
     input EditConstant {
         id: String!
         constant: String
         identifier: String
         inUse: Boolean
     }
+
     input AddConstant {
         constant: String!
         identifier: String!
         inUse: Boolean
     }
 
-    type DeleteConstantsType {
-        count: Int
+    type DeviationConstantsResponse {
+        data: [Constant]
+        count:Int
     }
 
-    type DeviationConstantsResponse {
-    data: [Constant]
-    cont:Int
-    }
     type Query {
         getConstant(id:String!): Constant
         getAllConstants(pagination: PaginationArguments):  DeviationConstantsResponse
@@ -33,6 +32,6 @@ export const deviationTypes = `#graphql
     type Mutation {
         editConstant(input: EditConstant): Constant
         addNewConstant(input: AddConstant): Constant
-        deleteConstants(constantsIds: [String!]!): DeleteConstantsType
+        deleteConstants(constantsIds: [String!]!): Int
     }
 `;
